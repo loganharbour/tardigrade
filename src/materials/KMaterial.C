d27b8a7d7de6061fa6cb59878c1295ccf864b7fe
Logan Harbour <loganhharbour@gmail.com>
2026-06-03 16:14:13 -0600

    Add coupled variable parameter

--- src/materials/KMaterial.C
+++ src/materials/KMaterial.C
@@ -7,6 +7,7 @@ KMaterial::validParams()
 {
   InputParameters params = Material::validParams();
   params.addParam<MaterialName>("k_name", "k", "The name of material property for k");
+  params.addRequiredCoupledVar("variable", "The name of the variable whose gradient is in the denom");
   return params;
 }
 
@@ -18,4 +19,5 @@ KMaterial::KMaterial(const InputParameters & parameters)
 void
 KMaterial::computeQpProperties()
 {
+    _k[_qp] = 0;
 }
